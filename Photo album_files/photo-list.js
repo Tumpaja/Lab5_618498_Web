27eadf7a2a19c0ab4acf557c247684252cf906b3
Tumpaja <apppthai337@gmail.com>
2023-09-18 13:48:27 +0700

    first commit

---
+++ Photo album_files/photo-list.js
@@ -0,0 +1,8 @@
+const PHOTO_LIST = [
+  'Photo album_files/deepdish.jpg',
+  'Photo album_files/flatbread.jpg',
+  'Photo album_files/frenchbread.jpg',
+  'Photo album_files/neapolitan.jpg',
+  'Photo album_files/nypizza.jpg',
+  'Photo album_files/squarepan.jpg'
+];
